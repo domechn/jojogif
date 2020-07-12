927e022f946cf45b5abf8457c1d0687e07f920d9
Flesh12138 <1577760218@qq.com>
2020-07-12 20:02:17 +0800

    minor improvements

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import click
 @click.option('--end', default=0, help='from which time to end generate gif', type=float)
 @click.option('--size', default='', help='the size of gif, width*height, ex: 500*500')
 @click.option('--jojo', default=False, help='add to be continued', type=bool)
-@click.option('--fps', default=10, help='fps of gif', type=int)
+@click.option('--fps', default=10, help='intended fps of gif', type=int)
 def main(path, out_path: str, begin, end: float, size: str, jojo: bool, fps: int):
     try:
         if not path:
@@ -26,9 +26,9 @@ def main(path, out_path: str, begin, end: float, size: str, jojo: bool, fps: int
 def parse_size(size: str):
     if not size:
         return 0, 0
-    sizes = size.upper().split('*')
+    sizes = size.split('*')
     if len(sizes) != 2:
-        raise RuntimeError("size is invalid, ex: 200x200")
+        raise RuntimeError("size is invalid, ex: 200*200")
     return int(sizes[0]), int(sizes[1])
 
 
--- video.py
+++ video.py
@@ -1,5 +1,4 @@
 import random
-import shutil
 import string
 import tempfile
 from base64 import b64decode
@@ -7,7 +6,7 @@ from base64 import b64decode
 import cv2
 import numpy as np
 import imageio
-from os import path,  listdir
+from os import listdir
 from utils import *
 from common import logo_base64
 
@@ -17,26 +16,23 @@ _to_be_continued_path = 'image/to_be_continued.png'
 
 class VideoService():
 
-    def __init__(self, pt: str, begin, end: float, width, height, fps: int):
+    def __init__(self, pt: str, begin: float, end: float, width, height, intended_fps: int):
         if not end:
-            end = 20
+            end = 20  # default: 20s
         self._path = pt
         self._begin = begin
         self._end = end
         self._width = width or 0
         self._height = height or 0
-        self._fps = fps
+        self._intended_fps = intended_fps
         self._to_dir = path.join(
             tempfile.gettempdir(), 'jojogif',
             ''.join(random.sample(string.ascii_letters + string.digits,
                                   _random_dir_name_len)))
-        self._timeF = int(fps / self._fps) or 1
-        true_fps = fps / self._timeF
-        self._gif_delay = 1 / true_fps
 
     def to_images(self):
         '''
-        Convert video to picture at frame rate
+        Convert video to picture
         '''
         c = 0
         print_step('begin to make video to images')
@@ -46,8 +42,12 @@ class VideoService():
             if vc.isOpened():
                 width = self._width or vc.get(cv2.CAP_PROP_FRAME_WIDTH)
                 height = self._height or vc.get(cv2.CAP_PROP_FRAME_HEIGHT)
-                fps = vc.get(cv2.CAP_PROP_FPS)
-                timeF = self._timeF
+                source_fps = vc.get(cv2.CAP_PROP_FPS)
+
+                timeF = round(source_fps / self._intended_fps) or 1
+                self._true_fps = source_fps / timeF
+                self._gif_delay = 1 / self._true_fps
+
             else:
                 raise RuntimeError("cannot open video")
             while True:
@@ -55,13 +55,13 @@ class VideoService():
                 if frame is None:
                     break
                 c += 1
-                if c < self._begin * fps:
+                if c < self._begin * source_fps:
                     continue
-                if c > self._end * fps:
+                if c > self._end * source_fps:
                     break
                 if c % timeF == 0:
                     frame = cv2.resize(
-                        frame, (width, height), interpolation=cv2.INTER_AREA)
+                        frame, (int(width), int(height)), interpolation=cv2.INTER_AREA)
 
                     cv2.imwrite(f'{self._to_dir}/{c}.jpg', frame)
                 cv2.waitKey(1)
@@ -71,7 +71,7 @@ class VideoService():
 
     def to_gif(self, jojo: bool, out_path: str):
         '''
-        Convert video to picture at frame rate
+        Convert video to picture
         '''
         def get_image_path(p: str) -> str:
             return path.join(self._to_dir, p)
@@ -97,14 +97,7 @@ class VideoService():
             imageio.mimsave(outfilename, frames, 'GIF', duration=self._gif_delay)
         finally:
             rm_dir_f(self._to_dir)
-
-    def blur_last_image(self, p: str):
-        last_image = cv2.imread(p)
-        last_image_grey = cv2.cvtColor(last_image, cv2.COLOR_RGB2GRAY)
-        last_image_blur = cv2.GaussianBlur(last_image_grey, (5, 5), 0)
-        temp_file_path = path.join(self._to_dir, 'temp_file.jpg')
-        self._temp_file = temp_file_path
-        cv2.imwrite(temp_file_path, last_image_blur)
+            print_step('true fps: %d' % round(self._true_fps))
 
     def add_tbc_logo(self, last_image_name, get_image_path) -> list:
         '''
@@ -117,12 +110,20 @@ class VideoService():
         self.blur_last_image(get_image_path(last_image_name))
         tp = cv2.imread(self._temp_file)
         res = list()
-        for i in range(10):
+        for i in range(round(self._true_fps)): # tbc image appears for about 1 second
             name = get_image_path(
                 add_number_file_name(last_image_name, i+1))
             cv2.imwrite(name, self.cover(con_image, tp))
             res.append(name)
         return res
+
+    def blur_last_image(self, p: str):
+        last_image = cv2.imread(p)
+        last_image_grey = cv2.cvtColor(last_image, cv2.COLOR_RGB2GRAY)
+        last_image_blur = cv2.GaussianBlur(last_image_grey, (5, 5), 0)
+        temp_file_path = path.join(self._to_dir, 'temp_file.jpg')
+        self._temp_file = temp_file_path
+        cv2.imwrite(temp_file_path, last_image_blur)
 
     def cover(self, s_img, l_img, x_offset=0, y_offset=0):
         '''
